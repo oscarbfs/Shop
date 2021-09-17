d9da57a6c6f28e833e6117f0b8f27819d13d5ab2
oscarbfs <oscar.borgesfs@gmail.com>
2021-09-17 16:50:34 -0300

    Itens do Carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
+          primarySwatch: Colors.green,
+          accentColor: Colors.cyan,
           fontFamily: 'Lato',
         ),
         home: ProductsOverviewPage(),
