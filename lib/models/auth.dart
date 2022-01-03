a041cf1b0debcc2d67222dca49a9ad0657a77350
oscarbfs <oscar.borgesfs@gmail.com>
2022-01-03 09:45:08 -0300

    Login de Usuário

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -7,16 +7,26 @@ class Auth with ChangeNotifier {
   static const _url =
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDGPWy6Br9n2I9St22fT5bggLXiSuEu5P4';
 
-  Future<void> signup(String email, String password) async {
-    final response = await http.post(
-      Uri.parse(_url),
-      body: jsonEncode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
-
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+            'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyDGPWy6Br9n2I9St22fT5bggLXiSuEu5P4',
+        response = await http.post(
+          Uri.parse(url),
+          body: jsonEncode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          }),
+        );
     print(jsonDecode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    _authenticate(email, password, 'signInWithPassword');
   }
 }
