52296c8df9fc10593c2cb0609933a6a5b9500828
oscarbfs <oscar.borgesfs@gmail.com>
2022-01-14 10:30:20 -0300

    Animação no Carregamento das Imagens

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -16,8 +16,9 @@ class ProductGridItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(product.imageUrl),
             fit: BoxFit.cover,
           ),
           onTap: () {
